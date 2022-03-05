0018cf01017c1dc2a84dc4d4aeba0e88356f8224
Ole Aamot <ole@gnome.org>
2022-03-05 14:27:37 +0100

    git.code.sf.net/p/gnomeradio/gnomeradio.org

---
+++ thesis.sh
@@ -0,0 +1,4 @@
+wget -O thesis.bib http://www.ping.uio.no/~oka/thesis/thesis.bib
+wget -O thesis.dvi http://www.ping.uio.no/~oka/thesis/thesis.dvi
+wget -O thesis.pdf http://www.ping.uio.no/~oka/thesis/thesis.pdf
+wget -O thesis.tex http://www.ping.uio.no/~oka/thesis/thesis.tex
